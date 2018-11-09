07da98dd9aa3e534334fff3cf911dae094194bde
lihuqiiang <44639024+z3989883@users.noreply.github.com>
2018-11-09 09:36:47 +0800

    Add files via upload

---
+++ fakala/js/index.js
@@ -0,0 +1,6 @@
+setInterval("bander()",300);
+function bander(){
+document.getElementById("banderText").style.opacity=1;
+document.getElementById("banderText").style.margin="20px";
+document.getElementById("banderText2").style.opacity=1;
+}
