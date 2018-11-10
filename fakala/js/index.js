48e171ad17e69396f4c250c5424dab5cd626559a
lihuqiiang <44639024+z3989883@users.noreply.github.com>
2018-11-10 17:55:31 +0800

    Add files via upload

--- fakala/js/index.js
+++ fakala/js/index.js
@@ -1,6 +1,23 @@
-setInterval("bander()",300);
-function bander(){
-document.getElementById("banderText").style.opacity=1;
-document.getElementById("banderText").style.margin="20px";
-document.getElementById("banderText2").style.opacity=1;
-}
+//bangder文字动态出现
+setTimeout(ziti,300);
+function ziti(){
+   document.getElementById("banderText").style.opacity=1;
+   document.getElementById("banderText").style.margin="20px";
+   document.getElementById("banderText2").style.opacity=1;}
+
+//渐变轮播图
+setInterval(flb,4000);
+var ds=document.getElementsByClassName("banner-cell");
+var i=1;
+function flb(){
+        if(i==ds.length){
+            ds[ds.length-1].style.opacity=0;
+            i=0;
+        }
+        if(i>=1){
+        ds[i-1].style.opacity = 0;
+        }
+        ds[i].style.opacity=1;
+        i++;
+
+    }
